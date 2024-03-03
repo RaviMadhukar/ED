c0469e4dbb89b71a6f57ab87e292e370444a21b6
RAvi <7ravi6kumar@gmail.com>
2024-03-03 21:44:49 +0530

    sample

--- js/main.js
+++ js/main.js
@@ -46,22 +46,3 @@ form.addEventListener('submit', e => {
         })
         .catch(error => console.error('Error!', error.message))
 })
-
-// -----------------------------------------------------------------Training
-
-const scriptUL = 'https://script.google.com/macros/s/AKfycbyqHJh6AabpkbOXURzn9HAumxrrUzN922UVhbVQMC3JuuiyIMMOYzBbkOd2s2-cCUPy/exec'
-const frm = document.forms['submit-to-google-sheet2']
-const mg =document.getElementById("mssg");
-
-frm.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptUL, { method: 'POST', body: new FormData(frm) })
-        .then(response => {
-            mg.innerHTML = "Message Save Successfully"
-            setTimeout(function(){
-                msg.innerHTML =""
-            },5000)
-            frm.reset()
-        })
-        .catch(error => console.error('Error!', error.message))
-})
